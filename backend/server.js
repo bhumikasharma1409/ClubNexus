e3f50446d3393af608c35bd93c19409b2d778d1f
Bhavya Jain <bhavya0194.becse24@chitkara.edu.in>
2025-11-27 10:33:20 +0530

    Cloud Database

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false, // Set to console.log to see raw SQL queries
   }
